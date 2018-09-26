f3bd5c400a597b7a1c8dfc8bb3ebe3ceb8fa47eb
Helena Rasche <hxr@hx42.org>
2018-09-26 21:36:11 +0200

    support finding unmanaged domains

--- bin/find-unmanaged.sh
+++ bin/find-unmanaged.sh
@@ -37,4 +37,28 @@ for resource in {volume,server,keypair,security\ group}; do
 done
 
 
+success "Checking domains"
+
+for domain in {Z391FYOSFHL9U7,Z3BOXJYLR7ZV7D,Z2LADCUB4BUBWX}; do
+    tmp_ids=`mktemp`
+    # Find records under this domain
+    aws route53 list-resource-record-sets --hosted-zone-id ${domain} | \
+        # Ignore SOA and NS records
+        jq '.ResourceRecordSets[] | select (.Type != "SOA") | select(.Type != "NS")' | \
+        # Reformat as a terraform ID
+        jq '. | "'${domain}'_" + .Name + "_" + .Type' -r | \
+        # domains from aws end in `.`
+        sed 's/\._/_/' > ${tmp_ids}
+
+    for id in `cat ${tmp_ids}`; do
+        grep --quiet ${id} ${managed_resources};
+        ec=$?
+        if (( $ec != 0 )); then
+            error "Unmanaged: ${id}"
+        fi
+    done
+
+    rm ${tmp_ids}
+done
+
 rm ${managed_resources}
