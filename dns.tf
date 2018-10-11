c3317f25d47c70b1391fe18844d190a03b54071a
Helena Rasche <hxr@hx42.org>
2018-10-11 10:20:25 +0200

    update records

--- dns.tf
+++ dns.tf
@@ -2,20 +2,27 @@
 
 resource "aws_route53_record" "usegalaxy-eu" {
   zone_id = "${var.zone_usegalaxy_eu}"
-  name  = "usegalaxy.eu"
+  name    = "usegalaxy.eu"
   type    = "A"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["${openstack_compute_instance_v2.proxy.access_ip_v4}"]
 }
 
 resource "aws_route53_record" "usegalaxy-de" {
   zone_id = "${var.zone_usegalaxy_de}"
-  name  = "usegalaxy.de"
+  name    = "usegalaxy.de"
   type    = "A"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["${openstack_compute_instance_v2.proxy.access_ip_v4}"]
 }
 
+resource "aws_route53_record" "galaxyproject-eu" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "galaxyproject.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.proxy.access_ip_v4}"]
+}
 
 # Subdomains are all just CNAMEs for galaxyproject.eu
 variable "subdomain" {
@@ -27,17 +34,20 @@ variable "subdomain" {
     "metagenomics.usegalaxy.eu",
     "rna.usegalaxy.eu",
     "build.usegalaxy.eu",
+    "stats.usegalaxy.eu",
+    "beta.usegalaxy.eu",
+    "proteomics.usegalaxy.eu",
   ]
 }
 
 resource "aws_route53_record" "subdomains" {
   zone_id = "${var.zone_usegalaxy_eu}"
 
-  count = 6
+  count = 8
   name  = "${element(var.subdomain, count.index)}"
 
   type    = "CNAME"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["proxy.galaxyproject.eu"]
 }
 
@@ -46,7 +56,7 @@ resource "aws_route53_record" "influxdb-usegalaxy-eu" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "influxdb.usegalaxy.eu"
   type    = "A"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["192.52.2.249"]
 }
 
@@ -54,22 +64,14 @@ resource "aws_route53_record" "proxy-usegalaxy-eu" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "proxy.usegalaxy.eu"
   type    = "A"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["192.52.3.222"]
-}
-
-resource "aws_route53_record" "stats-usegalaxy-eu" {
-  zone_id = "${var.zone_usegalaxy_eu}"
-  name    = "stats.usegalaxy.eu"
-  type    = "A"
-  ttl     = "300"
-  records = ["192.52.3.121"]
 }
 
 resource "aws_route53_record" "sentry-usegalaxy-eu" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "sentry.usegalaxy.eu"
   type    = "A"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["192.52.2.56"]
 }
--- dns_mail.tf
+++ dns_mail.tf
@@ -2,7 +2,7 @@ resource "aws_route53_record" "mx" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "usegalaxy.eu"
   type    = "MX"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["10 mxa.mailgun.org", "10 mxb.mailgun.org"]
 }
 
@@ -10,7 +10,7 @@ resource "aws_route53_record" "spf" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "usegalaxy.eu"
   type    = "TXT"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["v=spf1 include:mailgun.org ~all"]
 }
 
@@ -18,6 +18,6 @@ resource "aws_route53_record" "dkim" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "mailo._domainkey.usegalaxy.eu"
   type    = "TXT"
-  ttl     = "300"
+  ttl     = "7200"
   records = ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDWqARw6lby+yJNifDd1Nf9FfegF2TB4VJAdupceZ6pAHwYvS/2WFUwc3ZxdA8+awjDfLr6cAWERnP6YsBqZ7/1jCvtvOM2RLvsKMrzKG+KycfSfaGfFLZw/qPHM+82Mz7o5PdmcVehDSjKrnRXWGjliF/bazOoPg4OJObmAlhovwIDAQAB"]
 }
