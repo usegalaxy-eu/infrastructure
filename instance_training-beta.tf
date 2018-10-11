2279587a6cc64f414b43b9c27f26f2c6ffafcdcf
Helena Rasche <hxr@hx42.org>
2018-10-11 11:03:49 +0200

    test instance

--- instance_training-beta.tf
+++ instance_training-beta.tf
@@ -1,12 +1,12 @@
-resource "openstack_compute_instance_v2" "training-beca2018" {
-  name            = "vgcnbwc-training-beca2018-${count.index}"
+resource "openstack_compute_instance_v2" "training-beta" {
+  name            = "vgcnbwc-training-beta-${count.index}"
   image_name      = "${var.vgcn_image}"
   flavor_name     = "c.c10m55"
   key_pair        = "cloud2"
   security_groups = ["public"]
   count           = 1
 
-  user_data = "${file("conf/node.yml")}"
+  user_data = "${file("conf/node-new.yml")}"
 
   network {
     name = "bioinf"
