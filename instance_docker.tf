3606bc3a97896b40c957ac989919b671ddf280a1
Helena Rasche <hxr@hx42.org>
2018-11-02 15:18:48 +0100

    add dns for docker

--- instance_docker.tf
+++ instance_docker.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "docker-host" {
-  name            = "docker-host.usegalaxy.eu"
+  name            = "docker.galaxyproject.eu"
   image_name      = "${var.centos_image}"
   flavor_name     = "m1.xxlarge"
   key_pair        = "cloud2"
@@ -8,4 +8,12 @@ resource "openstack_compute_instance_v2" "docker-host" {
   network {
     name = "public"
   }
+}
+
+resource "aws_route53_record" "docker-host" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "docker.galaxyproject.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.docker-host.access_ip_v4}"]
 }
