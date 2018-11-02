a2829bc80a6086345a70a458d417a33907fd97c1
Helena Rasche <hxr@hx42.org>
2018-11-02 15:26:34 +0100

    docker open temporarily

--- instance_docker.tf
+++ instance_docker.tf
@@ -3,7 +3,7 @@ resource "openstack_compute_instance_v2" "docker-host" {
   image_name      = "${var.centos_image_new}"
   flavor_name     = "m1.xxlarge"
   key_pair        = "cloud2"
-  security_groups = "${var.sg_webservice}"
+  security_groups = "${var.sg_webservice-pubssh}"
 
   network {
     name = "public"
