d758be5c1f958d14078526f40f4a2788443f8cbc
Helena Rasche <hxr@hx42.org>
2018-11-06 12:36:07 +0100

    allocate more infra

--- instance_training.tf
+++ instance_training.tf
@@ -36,3 +36,25 @@ module "training-ucabgt2018" {
   galaxygroup = "training-ucabgt2018"
   is_training = "True"
 }
+
+module "training-big-data2018" {
+  # 11/19/2018
+  # 11/23/2018
+  source      = "modules/vgcn-node"
+  count       = 10
+  flavor      = "c.c16m120"
+  name        = "training-big-data2018"
+  galaxygroup = "training-big-data2018"
+  is_training = "True"
+}
+
+module "training-emc2018" {
+  # 11/22/2018
+  # 11/23/2018
+  source      = "modules/vgcn-node"
+  count       = 15
+  flavor      = "c.c16m120"
+  name        = "training-emc2018"
+  galaxygroup = "training-emc2018"
+  is_training = "True"
+}
