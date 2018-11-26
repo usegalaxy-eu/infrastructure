135aba43495259fc58ed5ae99479b9fa94c3a774
Helena Rasche <hxr@hx42.org>
2018-11-26 13:03:35 +0100

    trainings

--- instance_training.tf
+++ instance_training.tf
@@ -17,37 +17,22 @@ module "training-ucabgt2018" {
   is_training = "True"
 }
 
-# Until 2018-11-23
-module "training-big-data2018" {
-  # 11/19/2018
-  # 11/23/2018
-  source = "modules/vgcn-node"
-
-  count       = 10
-  flavor      = "c.c16m120"
-  name        = "training-big-data2018"
-  galaxygroup = "training-big-data2018"
-  is_training = "True"
-}
-
-# Until 2018-11-23
-module "training-emc2018" {
-  # 11/22/2018
-  # 11/23/2018
-  source = "modules/vgcn-node"
-
-  count       = 15
-  flavor      = "c.c16m120"
-  name        = "training-emc2018"
-  galaxygroup = "training-emc2018"
-  is_training = "True"
-}
-
 module "training-genomika2" {
   source      = "modules/vgcn-node"
   count       = 1
   flavor      = "c.c32m240"
   name        = "training-genomika2"
   galaxygroup = "training-genomika2"
+  is_training = "True"
+}
+
+module "training-bioinformatika2018" {
+# 11/26/2018 - 12/31/2018
+  source = "modules/vgcn-node"
+
+  count       = 2
+  flavor      = "c.c16m120"
+  name        = "training-bioinformatika2018"
+  galaxygroup = "training-bioinformatika2018"
   is_training = "True"
 }
