75a979762ad45eb6bc436e0cab7b03e52c1101ba
Helena Rasche <hxr@hx42.org>
2018-12-03 14:39:46 +0100

    blah

--- instance_training.tf
+++ instance_training.tf
@@ -1,4 +1,5 @@
 module "training-bioinfbrad2018_1" {
+  # Until 12/16/2018
   source      = "modules/vgcn-node"
   count       = 1
   flavor      = "c.c10m55"
@@ -7,27 +8,8 @@ module "training-bioinfbrad2018_1" {
   is_training = "True"
 }
 
-# Until 2018-11-25
-module "training-ucabgt2018" {
-  source      = "modules/vgcn-node"
-  count       = 4
-  flavor      = "c.c10m55"
-  name        = "training-ucabgt2018"
-  galaxygroup = "training-ucabgt2018"
-  is_training = "True"
-}
-
-module "training-genomika2" {
-  source      = "modules/vgcn-node"
-  count       = 1
-  flavor      = "c.c32m240"
-  name        = "training-genomika2"
-  galaxygroup = "training-genomika2"
-  is_training = "True"
-}
-
 module "training-bioinformatika2018" {
-# 11/26/2018 - 12/31/2018
+  # 12/31/2018
   source = "modules/vgcn-node"
 
   count       = 2
@@ -36,13 +18,13 @@ module "training-bioinformatika2018" {
   galaxygroup = "training-bioinformatika2018"
   is_training = "True"
 }
-module "training-msi_dresden" {
-  # 11/29/2018
+
+module "training-genomika3" {
+  # Until 12/4/2018
   source      = "modules/vgcn-node"
-  count       = 8
+  count       = 2
   flavor      = "c.c32m240"
-  name        = "trianing-msi_dresden"
-  galaxygroup = "training-msi_dresden"
+  name        = "training-genomika3"
+  galaxygroup = "training-genomika3"
   is_training = "True"
 }
-
--- instance_vgcn-central-manager.tf
+++ instance_vgcn-central-manager.tf
@@ -1,21 +1,21 @@
-resource "openstack_compute_instance_v2" "vgcn-cm" {
-  name            = "manager.vgcn.galaxyproject.eu"
-  image_name      = "${var.vgcn_image}"
-  flavor_name     = "m1.small"
-  key_pair        = "cloud2"
-  security_groups = ["public"]
+#resource "openstack_compute_instance_v2" "vgcn-cm" {
+  #name            = "manager.vgcn.galaxyproject.eu"
+  #image_name      = "${var.vgcn_image}"
+  #flavor_name     = "m1.small"
+  #key_pair        = "cloud2"
+  #security_groups = ["public"]
 
-  network {
-    name = "bioinf"
-  }
+  #network {
+    #name = "bioinf"
+  #}
 
-  user_data = "${file("conf/cm.yml")}"
-}
+  #user_data = "${file("conf/cm.yml")}"
+#}
 
-resource "aws_route53_record" "vgcn-cm" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "manager.vgcn.galaxyproject.eu"
-  type    = "A"
-  ttl     = "300"
-  records = ["${openstack_compute_instance_v2.vgcn-cm.access_ip_v4}"]
-}
+#resource "aws_route53_record" "vgcn-cm" {
+  #zone_id = "${var.zone_galaxyproject_eu}"
+  #name    = "manager.vgcn.galaxyproject.eu"
+  #type    = "A"
+  #ttl     = "300"
+  #records = ["${openstack_compute_instance_v2.vgcn-cm.access_ip_v4}"]
+#}
