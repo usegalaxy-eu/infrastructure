77b88dfe2f95faea6f153df1595de710df44c0f7
Helena Rasche <hxr@hx42.org>
2018-12-18 13:02:26 +0100

    correct secgroup

--- secgroup_ufr-influxdb.tf
+++ secgroup_ufr-influxdb.tf
@@ -1,6 +1,6 @@
 resource "openstack_networking_secgroup_v2" "ufr-influxdb" {
   name                 = "ufr-influxdb"
-  description          = "[tf] Allow public HTTP(s) connections to port 8086"
+  description          = "[tf] Allow UFR HTTP(s) connections to port 8086"
   delete_default_rules = "true"
 }
 
@@ -14,4 +14,21 @@ resource "openstack_networking_secgroup_rule_v2" "ufr-influxdb" {
 
   remote_ip_prefix = "${element(var.netz, count.index)}"
   count            = "${var.netz_count}"
+}
+
+resource "openstack_networking_secgroup_v2" "public-influxdb" {
+  name                 = "public-influxdb"
+  description          = "[tf] Allow public HTTP(s) connections to port 8086"
+  delete_default_rules = "true"
+}
+
+resource "openstack_networking_secgroup_rule_v2" "public-influxdb" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = "8086"
+  port_range_max    = "8086"
+  security_group_id = "${openstack_networking_secgroup_v2.public-influxdb.id}"
+
+  remote_ip_prefix = "0.0.0.0/0"
 }
