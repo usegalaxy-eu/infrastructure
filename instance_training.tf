d181de098ee250dd1cc47b65a7ff5d83e69cdcb2
Helena Rasche <hxr@hx42.org>
2019-01-07 13:27:05 +0100

    cleanup

--- instance_training.tf
+++ instance_training.tf
@@ -1,13 +1,3 @@
-module "training-bioinfbrad2018_1" {
-  # Until 12/16/2018
-  source      = "modules/vgcn-node"
-  count       = 1
-  flavor      = "c.c10m55"
-  name        = "training-bioinfbrad2018_1"
-  galaxygroup = "training-bioinfbrad2018_1"
-  is_training = "True"
-}
-
 module "training-bioinformatika2018" {
   # 12/31/2018
   source = "modules/vgcn-node"
