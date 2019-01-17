80a5b19d7b33a598490924ffed73c0f78bf98925
Helena Rasche <hxr@hx42.org>
2019-01-17 07:47:33 +0100

    add training vms

--- instance_training.tf
+++ instance_training.tf
@@ -1,1 +1,9 @@
-
+# Until 2018-01-19
+module "training-bioinformatika" {
+  source      = "modules/vgcn-node"
+  count       = 4
+  flavor      = "c.c40m1000"
+  name        = "training-bioinformatika"
+  galaxygroup = "training-bioinformatika"
+  is_training = "True"
+}
