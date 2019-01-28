e5601802880a6d40c70cad0d512620c75601a410
Helena Rasche <hxr@hx42.org>
2019-01-28 13:20:51 +0100

    rmeove

--- instance_test.tf
+++ instance_test.tf
@@ -37,26 +37,3 @@ resource "aws_route53_record" "helena-test" {
   ttl     = "7200"
   records = ["${openstack_compute_instance_v2.helena-test.access_ip_v4}"]
 }
-
-resource "openstack_compute_instance_v2" "jwolff-test" {
-  name            = "jwolff-test"
-  image_name      = "Ubuntu 18.04"
-  flavor_name     = "c.c40m1000"
-  key_pair        = "jwolff"
-  security_groups = "${var.sg_webservice-pubssh}"
-
-  network {
-    name = "public"
-  }
-}
-
-resource "openstack_blockstorage_volume_v2" "jwolff-test-data" {
-  name        = "jwolff-test"
-  description = "Data volume for test"
-  size        = 20
-}
-
-resource "openstack_compute_volume_attach_v2" "jwolff-test-va" {
-  instance_id = "${openstack_compute_instance_v2.jwolff-test.id}"
-  volume_id   = "${openstack_blockstorage_volume_v2.jwolff-test-data.id}"
-}
