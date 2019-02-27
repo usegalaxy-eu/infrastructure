c88a2d86e97b3286c6ddddd35363db097dd07820
Helena Rasche <hxr@hx42.org>
2019-02-27 14:14:17 +0100

    admin port not needed

--- secgroup_public-amqp.tf
+++ secgroup_public-amqp.tf
@@ -13,13 +13,3 @@ resource "openstack_networking_secgroup_rule_v2" "public-amqp-5671" {
   port_range_max    = "5671"
   security_group_id = "${openstack_networking_secgroup_v2.public-amqp.id}"
 }
-
-resource "openstack_networking_secgroup_rule_v2" "public-amqp-15672" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  remote_ip_prefix  = "0.0.0.0/0"
-  port_range_min    = "15672"
-  port_range_max    = "15672"
-  security_group_id = "${openstack_networking_secgroup_v2.public-amqp.id}"
-}
