183a4e7082932d2b2d150d144a9bf8c7b1a8a225
Helena Rasche <hxr@hx42.org>
2019-03-04 15:13:04 +0100

    add ftp server

---
+++ instance_ftp.tf
@@ -0,0 +1,21 @@
+resource "openstack_compute_instance_v2" "ftp" {
+  name        = "ftp.usegalaxy.eu"
+  image_name  = "${var.centos_image_new}"
+  flavor_name = "m1.small"
+  key_pair    = "cloud2"
+
+  # TODO: tighten up secgroups
+  security_groups = ["egress", "public"]
+
+  network {
+    name = "public-extended"
+  }
+}
+
+resource "aws_route53_record" "ftp" {
+  zone_id = "${var.zone_usegalaxy_eu}"
+  name    = "ftp.usegalaxy.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.ftp.access_ip_v4}"]
+}
