73d532717c4e619884b3ed0206e58ed7fd8e1fb9
Helena Rasche <hxr@hx42.org>
2019-03-04 16:19:51 +0100

    reorg dns

--- instance_ftp.tf
+++ instance_ftp.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "ftp" {
-  name        = "ftp.usegalaxy.eu"
+  name        = "ftp.internal.usegalaxy.eu"
   image_name  = "${var.centos_image_new}"
   flavor_name = "m1.small"
   key_pair    = "cloud2"
@@ -13,9 +13,17 @@ resource "openstack_compute_instance_v2" "ftp" {
 }
 
 resource "aws_route53_record" "ftp" {
-  zone_id = "${var.zone_usegalaxy_eu}"
-  name    = "ftp.usegalaxy.eu"
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "ftp.internal.galaxyproject.eu"
   type    = "A"
   ttl     = "7200"
   records = ["${openstack_compute_instance_v2.ftp.access_ip_v4}"]
+}
+
+resource "aws_route53_record" "ftp" {
+  zone_id = "${var.zone_usegalaxy_eu}"
+  name    = "ftp.usegalaxy.eu"
+  type    = "CNAME"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.proxy-internal.access_ip_v4}"]
 }
