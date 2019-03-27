295be9c2b4382af5ffb7ded8503b4a3b3ccce49f
Helena Rasche <hxr@hx42.org>
2019-03-27 12:55:00 +0100

    wip

--- instance_ftp.tf
+++ instance_ftp.tf
@@ -1,5 +1,5 @@
 resource "openstack_compute_instance_v2" "ftp" {
-  name        = "ftp.internal.usegalaxy.eu"
+  name        = "ftp.usegalaxy.eu"
   image_name  = "${var.centos_image_new}"
   flavor_name = "m1.small"
   key_pair    = "cloud2"
@@ -8,22 +8,14 @@ resource "openstack_compute_instance_v2" "ftp" {
   security_groups = ["egress", "public"]
 
   network {
-    name = "bioinf"
+    name = "public-extended"
   }
-}
-
-resource "aws_route53_record" "ftp-internal" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "ftp.internal.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["${openstack_compute_instance_v2.ftp.access_ip_v4}"]
 }
 
 resource "aws_route53_record" "ftp" {
   zone_id = "${var.zone_usegalaxy_eu}"
   name    = "ftp.usegalaxy.eu"
-  type    = "CNAME"
+  type    = "A"
   ttl     = "7200"
-  records = ["${openstack_compute_instance_v2.proxy-internal.access_ip_v4}"]
+  records = ["${openstack_compute_instance_v2.ftp.access_ip_v4}"]
 }
