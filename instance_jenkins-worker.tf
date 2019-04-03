07b366fcb451c4ab2dbb832c8492efda8cbe6be2
Helena Rasche <hxr@hx42.org>
2019-04-03 12:13:56 +0200

    add internal jenkins worker

--- instance_jenkins-worker.tf
+++ instance_jenkins-worker.tf
@@ -17,7 +17,7 @@ resource "openstack_compute_instance_v2" "jenkins-workers-internal" {
   flavor_name     = "m1.xlarge"
   key_pair        = "build-usegalaxy-eu"
   security_groups = ["public"]
-count = 1
+  count           = 1
 
   network {
     name = "public-extended"
