3d2dbf7e530fd2496b04279e444fb3dd304b9094
Helena Rasche <hxr@hx42.org>
2019-05-22 17:11:07 +0200

    try this

--- instance_job-working-dir.tf
+++ instance_job-working-dir.tf
@@ -1,7 +1,7 @@
 resource "openstack_compute_instance_v2" "nfs-server" {
   name            = "job-working-dir.internal.galaxyproject.eu"
   image_name      = "generic-centos7-v31-j18-2deef7cb2572-master"
-  flavor_name     = "m1.large"
+  flavor_name     = "test_qos_free"
   key_pair        = "cloud2"
   security_groups = ["public"]
 
@@ -22,6 +22,7 @@ resource "openstack_blockstorage_volume_v2" "nfs-server" {
   name        = "job-working-dir"
   description = "Data volume for job-working-dir"
   size        = "${1024 * 20}"
+  volume_type = "qos_free"
 }
 
 resource "openstack_compute_volume_attach_v2" "nfs-data-va" {
