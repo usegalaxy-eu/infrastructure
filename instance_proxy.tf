9abe5b84751a7f16abc09e1e45a38afbd632e7db
Helena Rasche <hxr@hx42.org>
2019-06-13 16:39:31 +0200

    remove our old proxy

--- instance_proxy.tf
+++ instance_proxy.tf
@@ -1,23 +1,3 @@
-resource "openstack_compute_instance_v2" "proxy" {
-  name            = "proxy.galaxyproject.eu"
-  image_name      = "${var.centos_image}"
-  flavor_name     = "m1.small"
-  key_pair        = "cloud2"
-  security_groups = ["egress", "public-ssh", "public-ping", "public-web2", "public-amqp"]
-
-  network {
-    name = "public"
-  }
-}
-
-resource "aws_route53_record" "proxy" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "proxy.galaxyproject.eu"
-  type    = "A"
-  ttl     = "300"
-  records = ["${openstack_compute_instance_v2.proxy.access_ip_v4}"]
-}
-
 resource "openstack_compute_instance_v2" "proxy-internal" {
   name            = "proxy.internal.galaxyproject.eu"
   image_name      = "${var.centos_image}"
@@ -35,5 +15,13 @@ resource "aws_route53_record" "proxy-internal" {
   name    = "proxy.internal.galaxyproject.eu"
   type    = "A"
   ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.proxy-internal.access_ip_v4}"]
+}
+
+resource "aws_route53_record" "proxy" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "proxy.galaxyproject.eu"
+  type    = "A"
+  ttl     = "300"
   records = ["${openstack_compute_instance_v2.proxy-internal.access_ip_v4}"]
 }
