a7a1f9b8610f891ead93f2f685d8b2411b12ea88
Helena Rasche <hxr@hx42.org>
2019-07-02 20:30:17 +0200

    fix resource name

--- instance_singularity-demo.tf
+++ instance_singularity-demo.tf
@@ -1,15 +1,14 @@
-resource "openstack_compute_keypair_v2" "cloud2" {
+resource "openstack_compute_keypair_v2" "mike" {
   name       = "mike"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDuz0HOGjW8WOY61tUao+0ZlVFRoJDBfn7tKLBQQMYwZwCWsHguZ5xXsKVzF0gWVlW80BEU4lkA7IrxUzA+fPgQHLJWZnu1X1ImLtcqNi8qF3OOndLPn0W3iBrpBL4K2R6F8xB9sh9ACxece0ABwvT8LGzmsv6B1m12Pq+7EcJBVrSvXmt7DUKP7xeM++jRtmnNzz5BoRLA54ZW2cyDjCwX4S2LUfza5iXc3GSUZPlkcTNUYxALbevpEZ7e+wny6iGKC2ubJVgwA6rDBaghyYPWeREbL85Q4H87nqmViex1jU1SfiSjGclj+yafZ7oesPFln36lEDMezF0m6ldfjaDF mike@mike7"
 }
-
 
 resource "openstack_compute_instance_v2" "gcc-demo" {
   name            = "${var.count}-singularity-demo.gcc.galaxyproject.eu"
   image_name      = "Ubuntu 18.04"
   flavor_name     = "m1.large"
   key_pair        = "mike"
-  count = 2
+  count           = 2
   security_groups = ["public"]
 
   network {
@@ -21,12 +20,11 @@ resource "openstack_blockstorage_volume_v2" "singularity-demo-data" {
   name        = "singularity-${var.count}"
   description = "Data volume for ${var.count}-singularity"
   size        = 50
-  count = 2
+  count       = 2
 }
 
 resource "openstack_compute_volume_attach_v2" "singularity-va" {
-  count = 2
-  volume_id = "${element(openstack_blockstorage_volume_v2.singularity-demo-data.*.id, count.index)}"
+  count       = 2
+  volume_id   = "${element(openstack_blockstorage_volume_v2.singularity-demo-data.*.id, count.index)}"
   instance_id = "${element(openstack_compute_instance_v2.gcc-demo.*.id, count.index)}"
 }
-
