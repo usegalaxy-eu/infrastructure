a38279e9d92f0dbaab0df8b52b852b14b6d214f3
Helena Rasche <hxr@hx42.org>
2019-07-03 18:17:43 +0200

    wipe out + replace

--- gcc2019-training.tf
+++ gcc2019-training.tf
@@ -4,12 +4,13 @@ resource "openstack_compute_keypair_v2" "gcc2019-training" {
 }
 
 variable "count" {
-  default = 15
+  default = 75
 }
 
 resource "random_pet" "training-vm" {
   keepers = {
     count = "${count.index}"
+    image = "Ubuntu 18.04"
   }
 
   length = 2
@@ -19,7 +20,7 @@ resource "random_pet" "training-vm" {
 resource "openstack_compute_instance_v2" "training-vm" {
   name            = "gcc-${count.index}.training.galaxyproject.eu"
   image_name      = "Ubuntu 18.04"
-  flavor_name     = "m1.small"
+  flavor_name     = "m1.medium"
   security_groups = ["public", "public-ping", "public-web2", "egress"]
 
   key_pair = "cloud2"
@@ -41,19 +42,6 @@ resource "openstack_compute_instance_v2" "training-vm" {
   EOF
 
   count = "${var.count}"
-}
-
-resource "aws_route53_record" "training-vm" {
-  zone_id = "Z391FYOSFHL9U7"
-  name    = "gcc-${count.index}.training.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["${element(openstack_compute_instance_v2.training-vm.*.access_ip_v4, count.index)}"]
-  count   = "${var.count}"
-}
-
-output "gcc2019_dns" {
-  value = ["${aws_route53_record.training-vm.*.name}"]
 }
 
 output "gcc2019_ips" {
