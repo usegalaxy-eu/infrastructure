34925f744c5818840e6aca4d4ddff622690db247
Helena Rasche <hxr@hx42.org>
2019-07-08 13:58:17 +0200

    workers for org

--- instance_jenkins-worker.tf
+++ instance_jenkins-worker.tf
@@ -23,3 +23,38 @@ resource "openstack_compute_instance_v2" "jenkins-workers-internal" {
     name = "public-extended"
   }
 }
+
+resource "openstack_compute_instance_v2" "jenkins-workers-org" {
+  name            = "worker-${count.index}.build.galaxyproject.eu"
+  image_name      = "Ubuntu 18.04"
+  flavor_name     = "m1.xxlarge"
+  key_pair        = "build-usegalaxy-org"
+  security_groups = ["public"]
+  count           = 2
+
+  network {
+    name = "public"
+  }
+}
+
+resource "openstack_blockstorage_volume_v2" "jenkins-workers-org-data" {
+  name        = "jenkins-org"
+  description = "Data volume for Jenkins @ usegalaxy.org"
+  size        = 200
+  count       = 2
+}
+
+resource "openstack_compute_volume_attach_v2" "jenkins-workers-org-va" {
+  instance_id = "${openstack_compute_instance_v2.jenkins-workers-org.*.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.jenkins-workers-org-data.*.id}"
+  count       = 2
+}
+
+resource "aws_route53_record" "jenkins-workers-org" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "n${var.count}.galaxyproject.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.jenkins-workers-org.*.access_ip_v4}"]
+  count   = 2
+}
