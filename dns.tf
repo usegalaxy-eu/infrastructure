6a9848d0eda72f50d6bb6ecd2cc32e32420a6411
Gianmauro Cuccuru <gmauro@gmail.com>
2019-08-19 17:39:11 +0200

    build is now bare metal

--- dns.tf
+++ dns.tf
@@ -160,3 +160,11 @@ resource "aws_route53_record" "vgcn-cm" {
   ttl     = "300"
   records = ["10.5.68.230"]
 }
+
+resource "aws_route53_record" "build-usegalaxy" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "build.galaxyproject.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["132.230.223.230"]
+}
