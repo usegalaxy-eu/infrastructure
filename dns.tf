e9feb084cdf363bcfd1245c948f099dfc0e316da
Helena Rasche <hxr@hx42.org>
2019-09-16 08:38:30 +0200

    move pulsar into separate

--- dns.tf
+++ dns.tf
@@ -114,46 +114,6 @@ resource "aws_route53_record" "ftp" {
   records = ["132.230.68.85"]
 }
 
-resource "aws_route53_record" "freiburg-pulsar" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "de01.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["192.52.34.116"]
-}
-
-resource "aws_route53_record" "tuebingen-pulsar" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "de02.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["193.196.20.125"]
-}
-
-resource "aws_route53_record" "bari-pulsar" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "it01.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["90.147.75.60"]
-}
-
-resource "aws_route53_record" "brussels-pulsar" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "be01.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["193.190.85.137"]
-}
-
-resource "aws_route53_record" "lisboa-pulsar" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "pt01.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["192.92.147.4"]
-}
-
 resource "aws_route53_record" "vgcn-cm" {
   zone_id = "${var.zone_galaxyproject_eu}"
   name    = "manager.vgcn.galaxyproject.eu"
@@ -168,12 +128,4 @@ resource "aws_route53_record" "build-usegalaxy" {
   type    = "A"
   ttl     = "7200"
   records = ["132.230.223.230"]
-}
-
-resource "aws_route53_record" "freiburg-pulsar-gpu" {
-  zone_id = "${var.zone_galaxyproject_eu}"
-  name    = "de03.pulsar.galaxyproject.eu"
-  type    = "A"
-  ttl     = "7200"
-  records = ["192.52.35.37"]
 }
