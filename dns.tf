9ba50af51a7d33796414c14b3df1d8c615ef7d37
Helena Rasche <hxr@hx42.org>
2019-09-16 08:41:25 +0200

    blah

--- dns.tf
+++ dns.tf
@@ -130,21 +130,20 @@ resource "aws_route53_record" "build-usegalaxy" {
   records = ["132.230.223.230"]
 }
 
-
 # Interactive Tools
-resource "aws_route53_record" "subdomains" {
+resource "aws_route53_record" "it-subdomain-main" {
   zone_id = "${var.zone_usegalaxy_eu}"
 
-  name  = "*.interactivetoolentrypoint.interactivetool.test.usegalaxy.eu"
+  name = "*.interactivetoolentrypoint.interactivetool.test.usegalaxy.eu"
 
   type    = "CNAME"
   ttl     = "7200"
   records = ["usegalaxy.eu"]
 }
 
-resource "aws_route53_record" "subdomains" {
+resource "aws_route53_record" "it-subdomain-test" {
   zone_id = "${var.zone_usegalaxy_eu}"
-  name  = "*.interactivetoolentrypoint.interactivetool.test.internal.usegalaxy.eu"
+  name    = "*.interactivetoolentrypoint.interactivetool.test.internal.usegalaxy.eu"
   type    = "CNAME"
   ttl     = "7200"
   records = ["${openstack_compute_instance_v2.test-galaxy.access_ip_v4}"]
