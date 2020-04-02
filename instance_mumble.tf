7450036524bbc47fbb3d1135d58730313e301f33
Gianmauro Cuccuru <gmauro@gmail.com>
2020-04-02 12:29:40 +0200

    add mumble instance

---
+++ instance_mumble.tf
@@ -0,0 +1,19 @@
+resource "openstack_compute_instance_v2" "mumble-usegalaxy" {
+  name            = "mumble.galaxyproject.eu"
+  image_name      = "Ubuntu 18.04"
+  flavor_name     = "m1.small"
+  key_pair        = "cloud2"
+  security_groups = "${var.sg_webservice-pubssh}"
+
+  network {
+    name = "public"
+  }
+}
+
+resource "aws_route53_record" "mumble-usegalaxy" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "mumble.galaxyproject.eu"
+  type    = "A"
+  ttl     = "7200"
+  records = ["${openstack_compute_instance_v2.mumble-usegalaxy.access_ip_v4}"]
+}
