dabaf08777bce4ca45355e371b94c49cbb3ecfb3
Gianmauro Cuccuru <gmauro@gmail.com>
2020-06-26 09:40:36 +0200

    add volume to test galaxy

--- instance_galaxy.tf
+++ instance_galaxy.tf
@@ -1,3 +1,7 @@
+variable "test-galaxy-volume-size" {
+  default = 100
+}
+
 resource "openstack_compute_instance_v2" "test-galaxy" {
   name            = "test.internal.usegalaxy.eu"
   image_name      = "${var.centos_image_new}"
@@ -8,6 +12,28 @@ resource "openstack_compute_instance_v2" "test-galaxy" {
   network {
     name = "bioinf"
   }
+
+  user_data = <<-EOF
+    #cloud-config
+    bootcmd:
+        - test -z "$(blkid /dev/vdb)" && mkfs -t ext4 /dev/vdb
+        - mkdir -p /opt/galaxy
+    mounts:
+        - ["/dev/vdb", "/opt/galaxy", auto, "defaults,nofail", "0", "2"]
+    runcmd:
+        - [ chown, "centos.centos", -R, /opt/galaxy ]
+  EOF
+}
+
+resource "openstack_blockstorage_volume_v2" "test-galaxy-volume" {
+  name        = "test-galaxy-volume"
+  description = "Data volume for test.usegalaxy.eu"
+  size        = "${var.test-galaxy-volume-size}"
+}
+
+resource "openstack_compute_volume_attach_v2" "test-galaxy-internal-va" {
+  instance_id = "${openstack_compute_instance_v2.test-galaxy.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.test-galaxy-volume.id}"
 }
 
 resource "aws_route53_record" "test-galaxy" {
