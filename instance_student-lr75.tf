cab25f97aea18ab87da66b7441d141e2b9cbd66e
Gianmauro Cuccuru <gmauro@gmail.com>
2020-11-03 22:26:37 +0100

    Update instance_student-lr75.tf

--- instance_student-lr75.tf
+++ instance_student-lr75.tf
@@ -50,7 +50,7 @@ resource "openstack_compute_instance_v2" "student-lr75" {
     users:
      - default
      - name: user
-       primary_group: user
+       shell: /bin/bash
        lock_passwd: true
        ssh_authorized_keys:
          - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC8MtPcL4AY97vB1tlDqKQteMUcBICmSDv4+oANJe61vwr/KmmnAi9oQIM/WV3jnnKlrX9rDSc9+9vOfddFp3vjq/nSQ0orq1jsrKsGKYvihBrf3cqMiog2GpA8honAFpSh479oVjLf+uyDJRpmVmf5oh9nLZxmmYd+mw+AkugsVj1gOuYamgmP2mBl8ViIXw6q4z4Bb7RNo8LtFa06mUFV//zT23+C5nVzN4xymVuVsUfPWvvyD3kzmqnML21Q7fXwSzeA/UjdJfwBvFJv4yzqFoWOC3gHHJl810afPQsERQQOHInoTJ6j0+VHX5Nxe7ckD33KDwdTsHhlRD9hJTRw/ueW7VAOyGdy4zrxYrOQHkbnP9C6i+GW6HK3xxCMZaLL5WKigEgcNr65gd4bebjBn0SmpxtVRjd+2Vi1W92PnCLVwqUXvxHSX4XKFuLo0sFoLIrfaIqA/51LYIg0QNIGq+vS1dDnEEUpJlSvXl90h39XC0dHyvwreAsUVmNSjAiZ6jhdzidcQ2Oc4u677NkmuvuAwA2NqsNbbPfHeDOHvawgPwQ+IUCJIdGegbwtgJ9S8Ov5YDA41dfdaDGlP+VKsmo+0Js1Rp1Fnc3i/kToKjAJVRusLMIWHvYaDNc7OLdyY9ALBJjkHP1Pg7V1iRYDzJk3nyG0Tlkr9lgpnbi3Vw=="
