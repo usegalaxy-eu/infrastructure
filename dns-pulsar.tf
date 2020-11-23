c3d5449326cc740eedca452d1afc9e18d620f9d6
Gianmauro Cuccuru <gmauro@gmail.com>
2020-11-23 13:56:32 +0100

    update de01 record

--- dns-pulsar.tf
+++ dns-pulsar.tf
@@ -3,7 +3,7 @@ resource "aws_route53_record" "freiburg-pulsar" {
   name    = "de01.pulsar.galaxyproject.eu"
   type    = "A"
   ttl     = "7200"
-  records = ["192.52.33.118"]
+  records = ["192.52.39.232"]
 }
 
 resource "aws_route53_record" "tuebingen-pulsar" {
