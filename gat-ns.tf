40c4574c3a88b939b45d3619e1a3ade7d34488d2
Helena Rasche <hxr@hx42.org>
2020-11-24 12:07:27 +0100

    hmm

--- gat-ns.tf
+++ gat-ns.tf
@@ -6,10 +6,10 @@ resource "aws_route53_zone" "training-gxp-eu" {
   name = "training.galaxyproject.eu"
 }
 
-resource "aws_route53_record" "ns-training-gxp-eu" {
-  zone_id = "${aws_route53_zone.gxp-eu.zone_id}"
-  name    = "training.galaxyproject.eu"
-  type    = "NS"
-  ttl     = "3600"
-  records = "${aws_route53_zone.training-gxp-eu.name_servers}"
-}
+#resource "aws_route53_record" "ns-training-gxp-eu" {
+  #zone_id = "${aws_route53_zone.gxp-eu.zone_id}"
+  #name    = "training.galaxyproject.eu"
+  #type    = "NS"
+  #ttl     = "3600"
+  #records = "${aws_route53_zone.training-gxp-eu.name_servers}"
+#}
