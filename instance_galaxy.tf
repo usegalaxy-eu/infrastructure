630eca9336ded8ed11a9ae2d29fd963abf6e700e
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-03-18 11:20:39 +0100

    fix label

--- instance_galaxy.tf
+++ instance_galaxy.tf
@@ -47,7 +47,7 @@ resource "openstack_blockstorage_volume_v2" "test-galaxy-cvmfs-cache-volume" {
   size        = "10"
 }
 
-resource "openstack_compute_volume_attach_v2" "test-galaxy-internal-va" {
+resource "openstack_compute_volume_attach_v2" "test-galaxy-internal-va2" {
   instance_id = "${openstack_compute_instance_v2.test-galaxy.id}"
   volume_id   = "${openstack_blockstorage_volume_v2.test-galaxy-cvmfs-cache-volume.id}"
 }
