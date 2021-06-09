0349eef0f59b180ce27e844d5317d24f169b96b5
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-06-09 18:24:38 +0200

    boot from volume

--- instance_galaxy.tf
+++ instance_galaxy.tf
@@ -1,55 +1,21 @@
-variable "test-galaxy_image" {
-  default = "CentOS 8"
-}
-
-variable "test-galaxy-volume-size" {
-  default = 100
-}
-
 resource "openstack_compute_instance_v2" "test-galaxy" {
   name            = "test.internal.usegalaxy.eu"
-  image_name      = "${var.test-galaxy_image}"
   flavor_name     = "m1.large"
   key_pair        = "cloud2"
   security_groups = ["egress", "public-web2", "ufr-ingress", "public-ping"]
 
+  block_device {
+    uuid                  = "594658a4-68c0-49ea-84bc-a973a6f5ffc7"
+    source_type           = "image"
+    volume_size           = 200
+    boot_index            = 0
+    destination_type      = "volume"
+    delete_on_termination = true
+  }
+
   network {
     name = "bioinf"
   }
-
-  user_data = <<-EOF
-    #cloud-config
-    bootcmd:
-        - test -z "$(blkid /dev/vdb)" && mkfs -t ext4 /dev/vdb && mkdir -p /opt/galaxy
-        - test -z "$(blkid /dev/vdc)" && mkfs -t ext4 /dev/vdc && mkdir -p /var/lib/cvmfs && chown -R cvmfs.cvmfs /var/lib/cvmfs
-    mounts:
-        - ["/dev/vdb", "/opt/galaxy", auto, "defaults,nofail", "0", "2"]
-        - ["/dev/vdc", "/var/lib/cvmfs", auto, "defaults,nofail", "0", "2"]
-  EOF
-}
-
-resource "openstack_blockstorage_volume_v2" "test-galaxy-volume" {
-  name        = "test-galaxy-volume"
-  description = "Data volume for test.usegalaxy.eu"
-  volume_type = "default"
-  size        = "${var.test-galaxy-volume-size}"
-}
-
-resource "openstack_compute_volume_attach_v2" "test-galaxy-internal-va" {
-  instance_id = "${openstack_compute_instance_v2.test-galaxy.id}"
-  volume_id   = "${openstack_blockstorage_volume_v2.test-galaxy-volume.id}"
-}
-
-resource "openstack_blockstorage_volume_v2" "test-galaxy-cvmfs-cache-volume" {
-  name        = "test-galaxy-cvmfs-cache-volume"
-  description = "CVMFS cache volume for test.usegalaxy.eu"
-  volume_type = "default"
-  size        = "10"
-}
-
-resource "openstack_compute_volume_attach_v2" "test-galaxy-internal-va2" {
-  instance_id = "${openstack_compute_instance_v2.test-galaxy.id}"
-  volume_id   = "${openstack_blockstorage_volume_v2.test-galaxy-cvmfs-cache-volume.id}"
 }
 
 resource "aws_route53_record" "test-galaxy" {
