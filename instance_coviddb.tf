4667920ba785a50d33b81dd683af842e9f58ad23
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-07-08 18:53:16 +0200

    add volume to coviddb

--- instance_coviddb.tf
+++ instance_coviddb.tf
@@ -21,7 +21,25 @@ resource "openstack_compute_instance_v2" "coviddb" {
      - default
        ssh_authorized_keys:
          - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDBYETX45v81szULejgdthTXJSsITJ/qGx8vLyCJtf+zV98ZYPH18Z5JxzkEAYqtlsGIqIv/4jYtASPWCYE+n2XS7HCvdZjjHmBsp3/567qlTRkWI4mrlB242+8tj0ZLbbKSO0tdVALjIZEbXhYz8E7qyA8hZYKJ+ZxXkAgcS0FiquuWPFJLhXugxVJfGX96XOJey+Ww5iRmnUsVYSe8qgq8F0qMVFJ3FmnmvVY1smHmeTmEKd9lr3ZxwT+ke6Jke4ZAzhaIY+sfOngQQhAKvHtyDdNTS8WFmGsfJ8e2jzCUinqbnZEUK7MzIeDN+omTT3vziEQSNKVFYDothPpNQ8YsV5cCiXmGv/QnARGNHlA1vu52zpX0y6sx/zF/jP58LZxkC+a3R6x8iW/r8OKZPZJkw20FpNK5Urtb+bC1ErLBtsR+pb8lGGExIwSQ516r06l6IEIeJZODeTgMQN4oEmNTnrYGGw7B9K0Vi485424EMy/Uj73OfMlqcEtDdYIyYwzDW8eI6aVFjIRPwSjKrl0oFGnwdIdr4zoYEkf1uY5KIBC1gO6p9LQogQpIV4T6uZ5dOkG1akSvxX/4fXQklnDWM8UWUmpdn3otRr++K2tSjQGRwB2Z5z1g+oAadVVzklL+CmjsO5J83s98JDUlAB+pVUZiCDvn/8eRFgXNtuKJw=="
+    bootcmd:
+        - test -z "$(blkid /dev/vdb)" && mkfs -t ext4 /dev/vdb
+        - mkdir -p /scratch
+    mounts:
+        - ["/dev/vdb", "/scratch", auto, "defaults,nofail", "0", "2"]
+    runcmd:
+        - [ chown, "centos.centos", -R, /scratch ]
   EOF
+}
+
+resource "openstack_blockstorage_volume_v2" "coviddb-volume" {
+  name        = "coviddb-volume"
+  description = "Data volume for ${var.coviddb-dns}"
+  size        = "100"
+}
+
+resource "openstack_compute_volume_attach_v2" "coviddb-internal-va" {
+  instance_id = "${openstack_compute_instance_v2.coviddb.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.coviddb-volume.id}"
 }
 
 resource "aws_route53_record" "coviddb" {
