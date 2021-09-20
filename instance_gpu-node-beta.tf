bbdf9975ae86d0ac7b566fbc8c10614591ce8924
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-09-20 12:14:25 +0200

    update

--- instance_gpu-node-beta.tf
+++ instance_gpu-node-beta.tf
@@ -1,14 +1,22 @@
-variable "gpu-node-beta-dns" {
+variable "gpu_node_beta_dns" {
   default = "gpu-node-beta.galaxyproject.eu"
 }
 
-data "openstack_images_image_v2" "gpu-node-beta-image" {
+resource "aws_route53_record" "gpunodebeta" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "${var.gpu_node_beta_dns}"
+  type    = "A"
+  ttl     = "600"
+  records = ["${openstack_compute_instance_v2.gpu-node-beta.access_ip_v4}"]
+}
+
+data "openstack_images_image_v2" "gpu_node_beta_image" {
   name = "vggp-v40-j238-9b9c0ecd5697-master"
 }
 
-resource "openstack_compute_instance_v2" "gpu-node-beta" {
-  name            = "${var.gpu-node-beta-dns}"
-  image_id        = "${data.openstack_images_image_v2.gpu-node-beta-image.id}"
+resource "openstack_compute_instance_v2" "gpu_node_beta" {
+  name            = "${var.gpu_node_beta_dns}"
+  image_id        = "${data.openstack_images_image_v2.gpu_node_beta_image.id}"
   flavor_name     = "g1.g1c40m110"
   key_pair        = "cloud2"
   security_groups = ["default", "public-ssh"]
@@ -18,7 +26,7 @@ resource "openstack_compute_instance_v2" "gpu-node-beta" {
   }
 
   block_device {
-    uuid                  = "${data.openstack_images_image_v2.gpu-node-beta-image.id}"
+    uuid                  = "${data.openstack_images_image_v2.gpu_node_beta-image.id}"
     source_type           = "image"
     volume_size           = 200
     destination_type      = "volume"
@@ -43,14 +51,15 @@ resource "openstack_compute_instance_v2" "gpu-node-beta" {
   EOF
 }
 
-resource "openstack_blockstorage_volume_v2" "gpu-node-beta-vol" {
-  name        = "gpu-node-beta-data-vol"
+resource "openstack_blockstorage_volume_v2" "gpu_node_beta_vol" {
+  name        = "gpu_node_beta_data_vol"
   volume_type = "default"
   description = "Data volume for gpu-node-beta"
   size        = 500
 }
 
 resource "openstack_compute_volume_attach_v2" "gpunodebeta-internal-va" {
-  instance_id = "${openstack_compute_instance_v2.gpu-node-beta.id}"
-  volume_id   = "${openstack_blockstorage_volume_v2.gpu-node-beta-vol.id}"
+  instance_id = "${openstack_compute_instance_v2.gpu_node_beta.id}"
+  volume_id   = "${openstack_blockstorage_volume_v2.gpu_node_beta_vol.id}"
 }
+
