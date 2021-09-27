ac2e47663d3bb86148ed760df2edb6c6764a3ed4
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-09-27 10:19:15 +0200

    add instance

---
+++ instance_incoming.tf
@@ -0,0 +1,25 @@
+variable "incoming-dns" {
+  default = "incoming.galaxyproject.eu"
+}
+
+resource "openstack_compute_instance_v2" "incoming" {
+  name        = "${var.incoming-dns}"
+  image_name  = "CentOS Stream 8"
+  flavor_name = "m1.small"
+  key_pair    = "cloud2"
+
+  # TODO: tighten up secgroups
+  security_groups = ["egress", "public"]
+
+  network {
+    name = "public-extended"
+  }
+}
+
+resource "aws_route53_record" "incoming" {
+  zone_id = "${var.zone_galaxyproject_eu}"
+  name    = "${var.incoming-dns}"
+  type    = "A"
+  ttl     = "600"
+  records = ["${openstack_compute_instance_v2.incoming.access_ip_v4}"]
+}
