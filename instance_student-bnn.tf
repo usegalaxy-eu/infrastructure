5c1458a9778ca28e87a313453800f1bd8104bd82
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-01-12 10:41:34 +0100

    update SSH key

--- instance_student-bnn.tf
+++ instance_student-bnn.tf
@@ -35,6 +35,6 @@ resource "openstack_compute_instance_v2" "student-bnn" {
     users:
      - default
     ssh_authorized_keys:
-     - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC8/9GdD96Xfc0MavIJQEuNSIrOlaBSYkH4s5RyGhNnc8Al7x3hC7KXt/mcqyNoI1JTvrXp23aDpTr67e3Dnrp6Me7z8nyY9oIy5WHOgwG2ra1Ga8oi89MVgXPhtxvcYnk8hVfuPDmhWkgwS5ILH/7V/1hEKHcS0H7Q0pJp8EGCBDFWrxNfUWqJJuVcXb11CMnaSJX/VhD+S4g4rG01lMr696+k8eKs9y2sq9JpuUD1TNN16RYu1uVlVz8nhIPbEBsnFeKV+EYKLcSRQnjDTNjWqZHHM9NMWfK3wGvr5pfla158ZdsYQTmQtcchTsEoB+7eAQaER3XsEtuo8yQj4zbL"
+     - "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCpsQe36ynq2Pi/HJkv1DvO+j/0vAEvTJE20e4C9O0fyy+1TcUC99mFedaPnbvMRzHh/XIGxsQcD0XrJSHT7fiT5pww88qkydtMySi24jzW/49IBhFsNqbvPlXOdYIVrXtlCbTBqmz4PHoe5mJlo7BjIeWAG2uhaZ/8ZwtbmGmrfj0MuA9yqCSzRbTJ/JesRY5/HIez92aspdHi/z8PUKgHK0hTLD7z2pkrH0YQGOv7STxI8u/ghFXUZ6y1CVjJj3+E/3pulo1Pk1tFLplSsJIyYw11RMu9jmXYTEMNAwdPOefABC+dx31rCklRGNT4DZsLjm1/rfWw2LlQsVQi3Hw19R8F8uhqirGtp8q0GkqgK+OIY4O04HQiMDofe+BDGOe67/KsLn41lHQwSCG3KR7oeggCf7Gr9E55NtzlWmj0Bovr6i1p86r3NBqMzT5guHYY1eZ3+uUTAO/9Q9TfqqbdaSypuCTJlnoOAtWZ3O/PyTLTPzP1sda1kzrwraNPSFU= benno@benno-uni"
   EOF
 }
