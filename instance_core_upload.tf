9de5261bfeed4cc9710b630614b31c237bf941ca
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-01-19 15:51:41 +0100

    add upload instance

--- instance_core_upload.tf
+++ instance_core_upload.tf
@@ -9,12 +9,12 @@ data "openstack_images_image_v2" "upload-image" {
 resource "openstack_compute_instance_v2" "upload" {
   name            = var.upload-dns
   image_id        = data.openstack_images_image_v2.upload-image.id
-  flavor_name     = "m1.small"
+  flavor_name     = "m1.medium"
   key_pair        = "cloud2"
   security_groups = ["default", "public-ssh", "public-web2", "ufr-ingress"]
 
   network {
-    name = "public-extended"
+    name = "bioinf"
   }
 
   user_data = <<-EOF
