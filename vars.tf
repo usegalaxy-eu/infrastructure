9949f847fa5dea8a52613aa4145d9df47bfc1377
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-01-19 15:46:33 +0100

    add vars

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "zone_galaxyproject_eu" {
+  default = "Z386N8B8JBC6TQ"
+}
+
+variable "zone_usegalaxy_eu" {
+  default = "Z1C7L7XFF9613J"
+}
+
+variable "netz_count" {
+  default = 3
+}
+
+variable "netz" {
+  description = "Internal networks"
+  type        = list(any)
+  default     = ["192.52.32.0/20", "10.0.0.0/8", "132.230.0.0/16"]
+}
