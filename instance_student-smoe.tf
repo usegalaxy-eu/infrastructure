cf1e71ce1004b43834d68c3e6833c1fac212e8e8
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-01-24 15:26:28 +0100

    add student instance

---
+++ instance_student-smoe.tf
@@ -0,0 +1,30 @@
+variable "student-smoe-dns" {
+  default = "student-smoe-vm"
+}
+
+data "openstack_images_image_v2" "student-smoe-image" {
+  name = "Ubuntu 20.04"
+}
+
+resource "openstack_compute_instance_v2" "student-smoe" {
+  name        = "${var.student-smoe-dns}"
+  flavor_name = "m1.xlarge"
+  key_pair    = "cloud2"
+  security_groups = [
+    "default",
+    "public-ssh"]
+
+  network {
+    name = "public-extended"
+  }
+
+  user_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+    users:
+     - default
+    ssh_authorized_keys:
+     - "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaDO2gPp78zX4VQahUxklW5hYvvOVO5SH9Lj9Pit+4q"
+  EOF
+}
