ca89c4b791302a40bd6cecfabab94cc012942f6c
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-03-11 21:10:43 +0100

    add apollo dns

--- dns.tf
+++ dns.tf
@@ -6,3 +6,12 @@ resource "aws_route53_record" "plausible" {
   ttl             = "600"
   records         = ["192.52.44.75"]
 }
+
+resource "aws_route53_record" "apollo-main" {
+  allow_overwrite = true
+  zone_id         = var.zone_galaxyproject_eu
+  name            = "apollo.internal.galaxyproject.eu"
+  type            = "A"
+  ttl             = "600"
+  records         = ["10.5.68.7"]
+}
