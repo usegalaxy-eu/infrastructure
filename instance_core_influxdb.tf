77e44c1740a7300d269b38402c619721237027dc
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-03-23 16:59:42 +0100

    update influxdb

--- instance_core_influxdb.tf
+++ instance_core_influxdb.tf
@@ -1,13 +1,26 @@
 resource "openstack_compute_instance_v2" "influxdb-usegalaxy" {
   name            = "influxdb.galaxyproject.eu"
-  image_name      = "generic-centos7-v31-j4-edc5aa3dc22c-master"
+  image_name      = "generic-rockylinux8-v60-j168-5333625af7b2-main"
   flavor_name     = "m1.xlarge"
   key_pair        = "cloud2"
-  security_groups = ["egress", "public-ssh", "public-ping", "public-influxdb", "public-web2"]
+  security_groups = ["egress", "public-ssh", "public-ping", "public-influxdb"]
 
   network {
     name = "public"
   }
+
+  user_data = <<-EOF
+    #cloud-config
+    bootcmd:
+        - test -z "$(blkid /dev/vdb)" && mkfs -t ext4 /dev/vdb
+        - mkdir -p /data
+    mounts:
+        - ["/dev/vdb", "/data", auto, "defaults,nofail", "0", "2"]
+    runcmd:
+        - [ chown, "centos.centos", -R, /data ]
+    package_update: true
+    package_upgrade: true
+  EOF
 }
 
 resource "openstack_blockstorage_volume_v2" "influxdb-data" {
