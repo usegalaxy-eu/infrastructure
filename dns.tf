52edc331d31f27baf61801a7723c73239381d8ed
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2022-03-24 19:00:20 +0100

    add usegalaxy.eu zone

--- dns.tf
+++ dns.tf
@@ -2,13 +2,14 @@ variable "sn06" {
   default = "132.230.223.239"
 }
 
-#resource "aws_route53_record" "usegalaxy-eu" {
-#  zone_id = var.zone_usegalaxy_eu
-#  name    = "usegalaxy.eu"
-#  type    = "A"
-#  ttl     = "7200"
-#  records = ["${var.sn06}"]
-#}
+resource "aws_route53_record" "usegalaxy-eu" {
+  allow_overwrite = true
+  zone_id         = var.zone_usegalaxy_eu
+  name            = "usegalaxy.eu"
+  type            = "A"
+  ttl             = "7200"
+  records         = ["${var.sn06}"]
+}
 
 resource "aws_route53_record" "galaxyproject-eu" {
   allow_overwrite = true
@@ -19,59 +20,60 @@ resource "aws_route53_record" "galaxyproject-eu" {
   records         = ["${var.sn06}"]
 }
 
-## Subdomains are all just CNAMEs for galaxyproject.eu → proxy-external
-#variable "subdomain" {
-#  type = list(string)
-#
-#  default = [
-#    # Please place new subdomains at the end of the list and increase the counter `count` below
-#    "ecology.usegalaxy.eu",
-#    "hicexplorer.usegalaxy.eu",
-#    "metagenomics.usegalaxy.eu",
-#    "rna.usegalaxy.eu",
-#    "build.usegalaxy.eu",
-#    "stats.usegalaxy.eu",
-#    "beta.usegalaxy.eu",
-#    "proteomics.usegalaxy.eu",
-#    "clipseq.usegalaxy.eu",
-#    "test.usegalaxy.eu",
-#    "old.usegalaxy.eu",
-#    "streetscience.usegalaxy.eu",
-#    "graphclust.usegalaxy.eu",
-#    "cheminformatics.usegalaxy.eu",
-#    "imaging.usegalaxy.eu",
-#    "singlecell.usegalaxy.eu",
-#    "climate.usegalaxy.eu",
-#    "nanopore.usegalaxy.eu",
-#    "metabolomics.usegalaxy.eu",
-#    "humancellatlas.usegalaxy.eu",
-#    "annotation.usegalaxy.eu",
-#    "erasmusmc.usegalaxy.eu",
-#    "live.usegalaxy.eu",
-#    "plants.usegalaxy.eu",
-#    "lite.usegalaxy.eu",
-#    "ml.usegalaxy.eu",
-#    "virology.usegalaxy.eu",
-#    "covid19.usegalaxy.eu",
-#    "africa.usegalaxy.eu",
-#    "microbiome.usegalaxy.eu",
-#    "cancer.usegalaxy.eu",
-#    "assembly.usegalaxy.eu",
-#    "india.usegalaxy.eu",
-#    "microgalaxy.usegalaxy.eu"
-#  ]
-#}
+# Subdomains are all just CNAMEs for galaxyproject.eu → proxy-external
+variable "subdomain" {
+  type = list(string)
 
-#resource "aws_route53_record" "subdomains" {
-#  zone_id = var.zone_usegalaxy_eu
-#
-#  count = 34
-#  name  = element(var.subdomain, count.index)
-#
-#  type    = "CNAME"
-#  ttl     = "7200"
-#  records = ["usegalaxy.eu"]
-#}
+  default = [
+    # Please place new subdomains at the end of the list and increase the counter `count` below
+    "ecology.usegalaxy.eu",
+    "hicexplorer.usegalaxy.eu",
+    "metagenomics.usegalaxy.eu",
+    "rna.usegalaxy.eu",
+    "build.usegalaxy.eu",
+    "stats.usegalaxy.eu",
+    "beta.usegalaxy.eu",
+    "proteomics.usegalaxy.eu",
+    "clipseq.usegalaxy.eu",
+    "test.usegalaxy.eu",
+    "old.usegalaxy.eu",
+    "streetscience.usegalaxy.eu",
+    "graphclust.usegalaxy.eu",
+    "cheminformatics.usegalaxy.eu",
+    "imaging.usegalaxy.eu",
+    "singlecell.usegalaxy.eu",
+    "climate.usegalaxy.eu",
+    "nanopore.usegalaxy.eu",
+    "metabolomics.usegalaxy.eu",
+    "humancellatlas.usegalaxy.eu",
+    "annotation.usegalaxy.eu",
+    "erasmusmc.usegalaxy.eu",
+    "live.usegalaxy.eu",
+    "plants.usegalaxy.eu",
+    "lite.usegalaxy.eu",
+    "ml.usegalaxy.eu",
+    "virology.usegalaxy.eu",
+    "covid19.usegalaxy.eu",
+    "africa.usegalaxy.eu",
+    "microbiome.usegalaxy.eu",
+    "cancer.usegalaxy.eu",
+    "assembly.usegalaxy.eu",
+    "india.usegalaxy.eu",
+    "microgalaxy.usegalaxy.eu"
+  ]
+}
+
+resource "aws_route53_record" "subdomains" {
+  allow_overwrite = true
+  zone_id         = var.zone_usegalaxy_eu
+
+  count = 34
+  name  = element(var.subdomain, count.index)
+
+  type    = "CNAME"
+  ttl     = "7200"
+  records = ["usegalaxy.eu"]
+}
 
 # Bare metals
 resource "aws_route53_record" "sn06-galaxyproject" {
