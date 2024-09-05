16789f402e456bb365b0276e89a1f857980d6f41
Mira Kuntz <mira.kuntz@yahoo.com>
2024-09-05 12:26:17 +0200

    remove old influx vm

--- instance_core_influxdb.tf
+++ instance_core_influxdb.tf
@@ -1,6 +1,6 @@
-resource "openstack_compute_instance_v2" "influxdb-usegalaxy" {
-  name            = "influxdb.galaxyproject.eu"
-  image_name      = "influxdb_15_04_2024"
+resource "openstack_compute_instance_v2" "influxdb-usegalaxy-new" {
+  name            = "influxdb-new.galaxyproject.eu"
+  image_name      = "vgcn~rockylinux-9-latest-x86_64~+generic+internal~20240821~36799~HEAD~5e494c7"
   flavor_name     = "m1.xlarge"
   key_pair        = "cloud2"
   security_groups = ["egress", "public-ssh", "public-ping", "public-influxdb", "public-web2"]
@@ -15,30 +15,35 @@ resource "openstack_compute_instance_v2" "influxdb-usegalaxy" {
 
   user_data = <<-EOF
     #cloud-config
+    bootcmd:
+        - test -z "$(blkid /dev/vdb)" && mkfs -t ext4 /dev/vdb
+        - mkdir -p /data
+    mounts:
+        - ["/dev/vdb", "/data", auto, "defaults,nofail", "0", "2"]
+    runcmd:
+        - [ chown, "centos.centos", -R, /data ]
     package_update: true
     package_upgrade: true
   EOF
 }
 
-# 17.4.2024: This resource creation is commented out because the volume
-# from the old cloud is being attached to the instance in the new cloud.
-# resource "openstack_blockstorage_volume_v3" "influxdb-data" {
-#   name        = "influxdb"
-#   description = "Data volume for InfluxDB"
-#   size        = 100
-# }
+ resource "openstack_blockstorage_volume_v3" "influxdb-data-new" {
+   name        = "influxdb-new"
+   description = "Data volume for InfluxDB"
+   size        = 200
+ }
 
-resource "openstack_compute_volume_attach_v2" "influxdb-va" {
-  instance_id = openstack_compute_instance_v2.influxdb-usegalaxy.id
-  volume_id   = "f6e77471-9180-4055-941f-b18fe353571c"
+resource "openstack_compute_volume_attach_v2" "influxdb-va-new" {
+  instance_id = openstack_compute_instance_v2.influxdb-usegalaxy-new.id
+  volume_id   = openstack_blockstorage_volume_v3.influxdb-data-new.id
   device      = "/dev/vdb"
 }
 
-// resource "aws_route53_record" "influxdb-usegalaxy-internal" {
-//   allow_overwrite = true
-//   zone_id         = var.zone_galaxyproject_eu
-//   name            = "influxdb.galaxyproject.eu"
-//   type            = "A"
-//   ttl             = "600"
-//   records         = ["${openstack_compute_instance_v2.influxdb-usegalaxy.access_ip_v4}"]
-// }
+resource "aws_route53_record" "influxdb-usegalaxy-internal-new" {
+  allow_overwrite = true
+  zone_id         = var.zone_galaxyproject_eu
+  name            = "influxdb.galaxyproject.eu"
+  type            = "A"
+  ttl             = "600"
+  records         = ["${openstack_compute_instance_v2.influxdb-usegalaxy-new.access_ip_v4}"]
+}
