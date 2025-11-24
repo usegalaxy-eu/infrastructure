441df5b1d689db6a908e104f2341d3b2de080c69
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2025-11-24 17:33:38 +0100

    Setup IAM keys to manage the GAT EU AWS Route53 zone's DNS records
    Allow GAT trainees to obtain wildcard certificates manipulating the Route53 DNS records, for example to set up Interactive Tools (see tutorial https://training.galaxyproject.org/training-material/topics/admin/tutorials/interactive-tools/tutorial.html).

--- gat-ns.tf
+++ gat-ns.tf
@@ -1,87 +1,71 @@
-#data "aws_route53_zone" "gxp-eu" {
-#  zone_id = aws_route53_zone.zone_galaxyproject_eu.zone_id
-#}
-#
-## Setup the hosted zone below
-#resource "aws_route53_zone" "training-gxp-eu" {
-#  name = "training.galaxyproject.eu"
-#}
-#
-## It needs its own set of NS
-#resource "aws_route53_record" "ns-training-gxp-eu" {
-#  zone_id = data.aws_route53_zone.gxp-eu.zone_id
-#  name    = "training.galaxyproject.eu"
-#  type    = "NS"
-#  ttl     = "3600"
-#  records = ["${aws_route53_zone.training-gxp-eu.name_servers}"]
-#}
-#
-#variable "count-iam-tokens" {
-#  default = 2
-#}
-#
-## Setup an IAM key
-#resource "aws_iam_access_key" "training-gxp-eu" {
-#  user    = element(aws_iam_user.training-gxp-eu.*.name, count.index)
-#  count   = var.count-iam-tokens
-#  pgp_key = file("gpg.pubkey")
-#}
-#
-## Output
-#output "gat-iam-key-access" {
-#  value     = ["${aws_iam_access_key.training-gxp-eu.*.id}"]
-#  sensitive = true
-#}
-#
-#output "gat-iam-key-secret" {
-#  value     = ["${aws_iam_access_key.training-gxp-eu.*.encrypted_secret}"]
-#  sensitive = true
-#}
-#
-## And the user
-#resource "aws_iam_user" "training-gxp-eu" {
-#  name  = "training.galaxyproject.eu@${count.index}"
-#  path  = "/"
-#  count = var.count-iam-tokens
-#}
-#
-## And setup their policy
-#resource "aws_iam_policy" "training-subdomain-access" {
-#  name        = "training-subdomain-access"
-#  path        = "/"
-#  description = "Permit training keys to access the training.galaxyproject.eu subdomain"
-#
-#  policy = <<EOF
-#{
-#    "Version": "2012-10-17",
-#    "Statement": [
-#        {
-#            "Effect": "Allow",
-#            "Action": [
-#                "route53:ListHostedZones",
-#                "route53:GetChange"
-#            ],
-#            "Resource": [
-#                "*"
-#            ]
-#        },
-#        {
-#            "Effect": "Allow",
-#            "Action": [
-#                "route53:ChangeResourceRecordSets"
-#            ],
-#            "Resource": [
-#                "arn:aws:route53:::hostedzone/${aws_route53_zone.training-gxp-eu.zone_id}"
-#            ]
-#        }
-#    ]
-#}
-#EOF
-#}
-#
-## Attach policy to user
-#resource "aws_iam_user_policy_attachment" "training-subdomain-access-gxp-eu" {
-#  user       = element(aws_iam_user.training-gxp-eu.*.name, count.index)
-#  policy_arn = aws_iam_policy.training-subdomain-access.arn
-#  count      = var.count-iam-tokens
-#}
+resource "aws_route53_record" "ns_training_galaxyproject_eu" {
+  zone_id = aws_route53_zone.zone_gat_eu.zone_id
+  name    = "training.galaxyproject.eu"
+  type    = "NS"
+  ttl     = "3600"
+  records = "${aws_route53_zone.zone_gat_eu.name_servers}"
+}
+
+variable "gat_count_iam_tokens" {
+  default = 28
+}
+
+resource "aws_iam_access_key" "training_galaxyproject_eu" {
+  user    = element(aws_iam_user.training_galaxyproject_eu.*.name, count.index)
+  count   = var.gat_count_iam_tokens
+}
+
+output "gat_iam_key_access" {
+  value     = ["${aws_iam_access_key.training_galaxyproject_eu.*.id}"]
+  sensitive = true
+}
+
+output "gat_iam_key_secret" {
+  value     = ["${aws_iam_access_key.training_galaxyproject_eu.*.encrypted_secret}"]
+  sensitive = true
+}
+
+resource "aws_iam_user" "training_galaxyproject_eu" {
+  name  = "training.galaxyproject.eu@${count.index}"
+  path  = "/"
+  count = var.gat_count_iam_tokens
+}
+
+resource "aws_iam_policy" "gat_subdomain_access" {
+  name        = "gat-subdomain-access"
+  path        = "/"
+  description = "Permit training keys to access the training.galaxyproject.eu subdomain"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "route53:ListHostedZones",
+                "route53:GetChange"
+            ],
+            "Resource": [
+                "*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "route53:ChangeResourceRecordSets"
+            ],
+            "Resource": [
+                "arn:aws:route53:::hostedzone/${aws_route53_zone.zone_gat_eu.zone_id}"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_user_policy_attachment" "training-subdomain-access-gxp-eu" {
+  user       = element(aws_iam_user.training_galaxyproject_eu.*.name, count.index)
+  policy_arn = aws_iam_policy.gat_subdomain_access.arn
+  count      = var.gat_count_iam_tokens
+}
