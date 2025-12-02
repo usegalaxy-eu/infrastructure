47e2020f831658805a204d01a3e6eca7b1a2a46f
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2025-12-02 15:43:59 +0100

    Decommission uploads VM in favor of KVM-based VM

--- dns.tf
+++ dns.tf
@@ -46,11 +46,10 @@ resource "aws_route53_record" "celery-galaxyproject" {
   records         = ["10.4.68.198"]
 }
 
-resource "aws_route53_record" "upload-kvm-galaxyproject" {
-  # temporary DNS record for the uploads server on KVM
+resource "aws_route53_record" "upload-galaxyproject" {
   allow_overwrite = true
   zone_id         = var.zone_galaxyproject_eu
-  name            = "upload-kvm.galaxyproject.eu"
+  name            = "upload.galaxyproject.eu"
   type            = "A"
   ttl             = "600"
   records         = ["10.4.68.194"]
