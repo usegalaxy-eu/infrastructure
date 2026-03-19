1a26d7ece7082b8d2e94a68103cdde640e19882b
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2026-03-19 15:32:10 +0100

    Create new Plausible VM with 35GB root disk

--- instance_core_plausible.tf
+++ instance_core_plausible.tf
@@ -18,9 +18,34 @@ resource "openstack_compute_instance_v2" "plausible-usegalaxy-old" {
   EOF
 }
 
-moved {
-  from = openstack_compute_instance_v2.plausible-usegalaxy
-  to   = openstack_compute_instance_v2.plausible-usegalaxy-old
+data "openstack_images_image_v2" "plausible-image" {
+  name = "plausible_16_04_2024"
+}
+
+resource "openstack_compute_instance_v2" "plausible-usegalaxy" {
+  name            = "plausible.galaxyproject.eu"
+  flavor_name     = "m1.large"
+  key_pair        = "cloud2"
+  security_groups = ["egress", "public-web2", "public-ftp", "default", "public-ping"]
+
+  network {
+    name = "public"
+  }
+
+  user_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+  EOF
+
+  block_device {
+    uuid                  = data.openstack_images_image_v2.plausible-image.id
+    source_type           = "image"
+    destination_type      = "volume"
+    boot_index            = 0
+    volume_size           = 35
+    delete_on_termination = true
+  }
 }
 
 resource "aws_route53_record" "plausible-usegalaxy" {
@@ -29,5 +54,5 @@ resource "aws_route53_record" "plausible-usegalaxy" {
   name            = "plausible.galaxyproject.eu"
   type            = "A"
   ttl             = "600"
-  records         = ["${openstack_compute_instance_v2.plausible-usegalaxy-old.access_ip_v4}"]
+  records         = ["${openstack_compute_instance_v2.plausible-usegalaxy.access_ip_v4}"]
 }
