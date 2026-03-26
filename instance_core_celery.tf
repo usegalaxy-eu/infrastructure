3c5b360c450ba569a4b5c6150cb0c55b45eb5c41
José Manuel Domínguez <dominguj@informatik.uni-freiburg.de>
2026-03-26 09:51:25 +0100

    Uncomment cloud Celery cluster

--- instance_core_celery.tf
+++ instance_core_celery.tf
@@ -1,46 +1,46 @@
-# variable "celery-count" {
-#   default = 0
-# }
-#
-# data "openstack_images_image_v2" "celery-image" {
-#   name = "Rocky 9.4"
-# }
-#
-# resource "openstack_compute_instance_v2" "celery" {
-#   name            = "celery-cloud-${count.index}.galaxyproject.eu"
-#   flavor_name     = "m1.xxlarge"
-#   key_pair        = "cloud2"
-#   tags            = []
-#   security_groups = ["default", "ingress-from-proxy"]
-#
-#   network {
-#     name = "bioinf"
-#   }
-#
-#   user_data = <<-EOF
-#     #cloud-config
-#     package_update: true
-#     package_upgrade: true
-#   EOF
-#
-#   block_device {
-#     uuid                  = data.openstack_images_image_v2.celery-image.id
-#     source_type           = "image"
-#     destination_type      = "volume"
-#     boot_index            = 0
-#     volume_size           = 35
-#     delete_on_termination = true
-#   }
-#
-#   count = var.celery-count
-# }
-#
-# resource "aws_route53_record" "celery-cloud-galaxyproject" {
-#   allow_overwrite = true
-#   zone_id         = var.zone_galaxyproject_eu
-#   name            = "celery-cloud-${count.index}.galaxyproject.eu"
-#   type            = "A"
-#   ttl             = "600"
-#   records         = ["${element(openstack_compute_instance_v2.celery.*.access_ip_v4, count.index)}"]
-#   count           = var.celery-count
-# }
+variable "celery-count" {
+  default = 0
+}
+
+data "openstack_images_image_v2" "celery-image" {
+  name = "Rocky 9.4"
+}
+
+resource "openstack_compute_instance_v2" "celery" {
+  name            = "celery-cloud-${count.index}.galaxyproject.eu"
+  flavor_name     = "m1.xxlarge"
+  key_pair        = "cloud2"
+  tags            = []
+  security_groups = ["default", "ingress-from-proxy"]
+
+  network {
+    name = "bioinf"
+  }
+
+  user_data = <<-EOF
+    #cloud-config
+    package_update: true
+    package_upgrade: true
+  EOF
+
+  block_device {
+    uuid                  = data.openstack_images_image_v2.celery-image.id
+    source_type           = "image"
+    destination_type      = "volume"
+    boot_index            = 0
+    volume_size           = 35
+    delete_on_termination = true
+  }
+
+  count = var.celery-count
+}
+
+resource "aws_route53_record" "celery-cloud-galaxyproject" {
+  allow_overwrite = true
+  zone_id         = var.zone_galaxyproject_eu
+  name            = "celery-cloud-${count.index}.galaxyproject.eu"
+  type            = "A"
+  ttl             = "600"
+  records         = ["${element(openstack_compute_instance_v2.celery.*.access_ip_v4, count.index)}"]
+  count           = var.celery-count
+}
