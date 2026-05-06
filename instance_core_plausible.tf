649bc8fba045cdea33c13bed0c546d70d2664f90
Mira <86979912+mira-miracoli@users.noreply.github.com>
2026-05-06 10:21:08 +0200

    Remove errored plausible
    plausible-old is the currently working instance.
    It will be migrated to KVM soon

--- instance_core_plausible.tf
+++ instance_core_plausible.tf
@@ -22,32 +22,6 @@ data "openstack_images_image_v2" "plausible-image" {
   name = "plausible_16_04_2024"
 }
 
-resource "openstack_compute_instance_v2" "plausible-usegalaxy" {
-  name            = "plausible.galaxyproject.eu"
-  flavor_name     = "m1.large"
-  key_pair        = "cloud2"
-  security_groups = ["egress", "public-web2", "public-ftp", "default", "public-ping"]
-
-  network {
-    name = "public"
-  }
-
-  user_data = <<-EOF
-    #cloud-config
-    package_update: true
-    package_upgrade: true
-  EOF
-
-  block_device {
-    uuid                  = data.openstack_images_image_v2.plausible-image.id
-    source_type           = "image"
-    destination_type      = "volume"
-    boot_index            = 0
-    volume_size           = 35
-    delete_on_termination = true
-  }
-}
-
 resource "aws_route53_record" "plausible-usegalaxy" {
   allow_overwrite = true
   zone_id         = var.zone_galaxyproject_eu
